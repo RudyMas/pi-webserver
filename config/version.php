045e91b61e32a7331e6d0298e2da8ebd76192540
Rudy Mas <rudy@digitalstock.be>
2020-08-10 15:28:47 +0200

    Version

--- config/version.php
+++ config/version.php
@@ -2,7 +2,7 @@
 /**
 * Version settings for the Alpha, Beta and Production releases of the website
 */
-define('NAME_WEBSITE', 'Name of the Website');
+define('NAME_WEBSITE', 'Pi Webserver CMS');
 define('VERSION_ALPHA', '0.0.1'); // Pre-Release development
-define('VERSION_BETA', '0.0.1'); // Beta-Release development
-define('VERSION', '0.0.1'); // Production-Release
+define('VERSION_BETA', '-'); // Beta-Release development
+define('VERSION', '-'); // Production-Release
